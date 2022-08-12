aec9d4f7a168fb89a574421f9f36598093fc981d
Kaz Che <kaz.che@gmail.com>
2022-08-12 00:34:18 -0700

    pulling in metadata from gatsy-config as graphql query into the page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteMetadata: {
         siteURL: 'https://www.somedomain.tld',
-        title: 'Intro Gtasby baby',
+        title: 'Intro Gtasby KIMIA',
         description: 'Can\'t sleep so let\'s learn something',
         image: 'https://americanlit309fall13.files.wordpress.com/2013/12/gatsby.jpg'
     },
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,28 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 export default function IndexPage() {
+    const data = useStaticQuery(graphql `
+    query GetSiteTitle {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }      
+    `);
+
+    const meta = data?.site?.siteMetadata ?? {};
+
     return (
+        <>
+        <header>
+            <Link to="/">{meta.title}</Link>
+        </header>
         <main>
             <h1> Hello This is Cyrus' Home Pager</h1>
             <Link to="/about">Like to know more about?</Link>
         </main>
+        </>
     )
 }
