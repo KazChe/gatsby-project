ebc1b5d72fd96c9c5510babe708fb19b0e40a7ff
Kaz Che <kaz.che@gmail.com>
2022-08-11 23:24:16 -0700

    use of gatsby Link component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function AboutPage() {
     return (
         <main>
-            <h1> How About This</h1>
+            <h1> How About This Site</h1>
+            <Link to="/">Go Homes</Link>
         </main>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 export default function IndexPage() {
     return (
         <main>
-            <h1> Hello Cyrus</h1>
+            <h1> Hello This is Cyrus' Home Pager</h1>
+            <Link to="/about">Like to know more about?</Link>
         </main>
     )
 }
