daf91132c8b61c6089e30805ccaac668529f4150
Kaz Che <kaz.che@gmail.com>
2022-08-12 00:03:00 -0700

    adding gatsby-config - play in GraphiQL

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+        siteURL: 'https://www.somedomain.tld',
+        title: 'Intro Gtasby baby',
+        description: 'Can\'t sleep so let\'s learn something',
+        image: 'https://americanlit309fall13.files.wordpress.com/2013/12/gatsby.jpg'
+    },
+
+};
