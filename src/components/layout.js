3ce1b29aa9f6dcda1a51a405607e8392d79985f7
Kaz Che <kaz.che@gmail.com>
2022-08-12 22:23:53 -0700

    consolidate seo, headers, meta data calls in to a Layout component

---
+++ src/components/layout.js
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import { Seo } from "./seo";
+
+export function Layout({
+    children,
+    title = false,
+    description = false,
+    image = false,
+}) {
+    const data = useStaticQuery(graphql `
+    query GetSiteTitle {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }      
+    `);
+
+    const meta = data?.site?.siteMetadata ?? {}; 
+
+    return (
+        <>
+        <Seo title={title} description={description} image={image} />
+        <header>
+            <Link to="/">{meta.title}</Link>
+            <nav>
+                <Link to="/about">About</Link>
+            </nav>
+        </header>
+        <main>
+            {children}
+        </main>
+        </>
+    )
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import { Seo } from "../components/seo";
 import { Link } from 'gatsby';
+import { Layout } from "../components/layout";
 
 export default function AboutPage() {
     return (
-        <>
-        <Seo title="About this site" description="More Info about this site"/>
-        <main>
+        <Layout title="About this site huh?" description="More information about this site">
             <h1> How About This Site</h1>
             <Link to="/">Go Homes</Link>
-        </main>
-        </>
+        </Layout>
     )
 }
